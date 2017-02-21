0a8da4bfd9c0827edf404e8fa615c1c09dba867e
Roman Kotchetov <romank@vacasa.com>
2017-02-20 20:13:19 -0800

    Added a new subject form

--- src/App.js
+++ src/App.js
@@ -4,45 +4,65 @@ import Subject from './Subject';
 import resources from './resources.json';
 
 class App extends Component {
-    // Blueprint (features, parts, pieces)
     constructor(props) {
         super(props);
         
-        this.handleEventClick = this.handleEventClick.bind(this);
         
         // console.log(Json);
+        
         this.state = {
-            resources: resources
+            resources: resources,
         }
         
+        this.handleEventClick = this.addNewSubject.bind(this);
         this.addNewResource = this.addNewResource.bind(this);
+        this.addNewSubject = this.addNewSubject.bind(this);
+        this.handleTypingNew = this.handleTypingNew.bind(this);
     }
     
     addNewResource(subject, x){
-        // const tempState = this.state;
-        // tempState.resources[subject].resources.push(x);
-        // this.setState(tempState);
-        
         let tempState = this.state;
         tempState.resources[subject].resources.push(x);
         this.setState(tempState);
         
-        fetch('../resources.json', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                tempState
-            })
-        });
-        
+        // fetch('../resources.json', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Accept': 'application/json',
+        //         'Content-Type': 'application/json',
+        //     },
+        //     body: JSON.stringify({
+        //         tempState
+        //     })
+        // });
     }
     
-    handleEventClick() {
-        alert("Nope");
+    handleTypingNew(e) {
+		const target = e.target;
+		const value = target.value;
+		const name = target.name;
+		
+		this.setState({
+			[e.target.name]: e.target.value
+		})
+	}
+    
+    addNewSubject(e) {
+        e.preventDefault();
+        
+        const tempState = this.state;
+        const newSubject = {
+            subject: this.state.subject,
+            resources: []
+        }
+                
+        this.setState.tempState;
+        tempState.resources.push(newSubject);
+        
+        console.log(tempState);
+        // console.log(newSubject);
     }
+    
     render() {
         return (
             <div>
@@ -55,7 +75,37 @@ class App extends Component {
                     }
                 )}
                 <br />
-                <button onClick={this.handleEventClick}>Hello</button>
+                <hr />
+                <br />
+                <form className="new-form">
+					<label htmlFor="subject"></label>
+					<input
+						name="subject"
+                        onChange={this.handleTypingNew}
+						value={this.state.subject}
+						type="text"
+                        placeholder="Subject"
+					/>
+                    <label htmlFor="title"></label>
+                    <input
+                        name="title"
+                        onChange={this.handleTypingNew}
+                        value={this.state.title}
+                        type="text"
+                        placeholder="Title"
+                    />
+                    <label htmlFor="url"></label>
+					<input
+						name="url"
+						onChange={this.handleTypingNew}
+						value={this.state.url}
+						type="text"
+                        placeholder="url ex: google.com"
+					/>
+                    <button onClick={this.addNewSubject}>
+						Submit
+					</button>
+				</form>
             </div>
         );
     }
